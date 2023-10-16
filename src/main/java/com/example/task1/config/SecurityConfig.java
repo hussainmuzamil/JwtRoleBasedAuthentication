525d8c67f306266abf9d1954bdf1dbdd8f103207
MuzamilHussain <hussainmuzamil199@gmail.com>
2023-10-17 01:41:13 +0500

    added security filter chain

--- src/main/java/com/example/task1/config/SecurityConfig.java
+++ src/main/java/com/example/task1/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.task1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -12,6 +14,29 @@ import java.net.http.HttpRequest;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+//    private final JWTHelper jwtHelper;
+    public SecurityConfig(
+            @Autowired JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint
+//            @Autowired JWTHelper jwtHelper
+    ){
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+//        this.jwtHelper = jwtHelper;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests().
+                requestMatchers("/users").authenticated().requestMatchers("/auth/users").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 
 }
